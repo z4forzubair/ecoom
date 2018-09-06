933b148284450dc62070e5d42e6eab1e855bdff4
zubair <muhammad.zubair@devsinc.com>
2018-09-06 12:54:46 +0500

    migrations done but devise to be added yet

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,99 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_09_06_073533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "images", force: :cascade do |t|
+    t.binary "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "imageable_type"
+    t.bigint "imageable_id"
+    t.index ["imageable_type", "imageable_id"], name: "index_images_on_imageable_type_and_imageable_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "no_of_products"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.integer "total_discount"
+    t.integer "total_bill"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "product_orders", force: :cascade do |t|
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "order_id"
+    t.bigint "product_id"
+    t.integer "discount"
+    t.index ["order_id"], name: "index_product_orders_on_order_id"
+    t.index ["product_id"], name: "index_product_orders_on_product_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.boolean "flag"
+    t.integer "count"
+    t.decimal "cost"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "added_by_user_id"
+    t.bigint "deleted_by_user_id"
+    t.integer "discount"
+    t.index ["added_by_user_id"], name: "index_products_on_added_by_user_id"
+    t.index ["deleted_by_user_id"], name: "index_products_on_deleted_by_user_id"
+  end
+
+  create_table "ratings", force: :cascade do |t|
+    t.integer "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.string "rated_type"
+    t.bigint "rated_id"
+    t.index ["rated_type", "rated_id"], name: "index_ratings_on_rated_type_and_rated_id"
+    t.index ["user_id"], name: "index_ratings_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "review"
+    t.bigint "comment_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.bigint "product_id"
+    t.index ["comment_id"], name: "index_reviews_on_comment_id"
+    t.index ["product_id"], name: "index_reviews_on_product_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.text "address"
+    t.string "contact_number"
+    t.binary "photo"
+    t.string "user_role"
+    t.boolean "flag"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "orders", "users"
+  add_foreign_key "product_orders", "orders"
+  add_foreign_key "product_orders", "products"
+  add_foreign_key "products", "users", column: "added_by_user_id"
+  add_foreign_key "products", "users", column: "deleted_by_user_id"
+  add_foreign_key "ratings", "users"
+  add_foreign_key "reviews", "products"
+  add_foreign_key "reviews", "users"
 end
